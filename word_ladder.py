55dd0ea354a97cd2f1f00aa99b2092177b1f43bc
Dhruv <dhruvdesmond@gmail.com>
2020-07-02 11:34:07 +0530

    word ladder

--- word_ladder.py
+++ word_ladder.py
@@ -1,3 +1,37 @@
+from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordDict = self.construct_dict(wordList)
+        if endWord not in wordList or not endWord or not beginWord or not wordList:
+            return 0
+        L = len(beginWord)
         
+        q = deque([(beginWord,1)])
+        visited = {}
+        
+        while q:
+            first = q.popleft()
+            print(first)
+            word,lvl = first
+            if word not in visited:
+                visited[word] = 1
+                if word == endWord:
+                    return lvl
+                for i in range(len(word)):
+                    curr = word[:i] + "_" + word[i+1:]
+                    neigh = wordDict.get(curr,[])
+                    for n in neigh:
+                        if n not in visited:
+                            q.append((n,lvl+1))
+        
+        return(0)
+        
+        
+        
+    def construct_dict(self,word_list):
+        d = {}
+        for word in word_list:
+            for i in range(len(word)):
+                s = word[:i] + "_" + word[i+1:]
+                d[s] = d.get(s, []) + [word]
+        return d
